072dc8d92df60b845fd0cc9c470a91a737a39580
Roberto Borges <roberto.borges@outlook.com>
2023-10-03 12:35:00 -0400

    removing the aditional schema

--- event-hubs-hec/helpers/splunk.js
+++ event-hubs-hec/helpers/splunk.js
@@ -74,7 +74,7 @@ const getSource = function(message) {
 
 const getEventString = function(record) {
 
-  eventString = `<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'><System><Provider Name='${record["Source"]}'/><EventID Qualifiers='${record["EventCategory"]}'>${record["EventID"]}</EventID><Level>${record["EventLevel"]}</Level><TimeCreated SystemTime='${record["TimeGenerated"]}'/><Channel>${record["EventLog"]}</Channel><Computer>${record["Computer"]}</Computer><Security UserID='${record["UserName"]}'/></System><EventData>${record["EventData"]}</EventData></Event>`;
+  eventString = `<Event><System><Provider Name='${record["Source"]}'/><EventID Qualifiers='${record["EventCategory"]}'>${record["EventID"]}</EventID><Level>${record["EventLevel"]}</Level><TimeCreated SystemTime='${record["TimeGenerated"]}'/><Channel>${record["EventLog"]}</Channel><Computer>${record["Computer"]}</Computer><Security UserID='${record["UserName"]}'/></System><EventData>${record["EventData"]}</EventData></Event>`;
   
   return eventString;
 }
